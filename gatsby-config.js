f4f2f4c0ae082544770e4410d7e06094ed653885
daniel1890 <danielroth1890@gmail.com>
2021-06-23 00:44:01 +0200

    add graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        collectionTypes: [
+          "project",
+        ],
+        queryLimit: 1000,
+      },
+    },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,38 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
+const IndexPage = () => {
+  return(
+    <StaticQuery
+      query={query}
+      render={data => (
+        <ul>
+          {data.allStrapiProject.edges.map(project => (
+            <li key={project.node.strapiId}>{project.node.name}</li>
+          ))}
+        </ul>
+      )}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+  )
+}
+
+const query = graphql`
+  {
+    allStrapiProject {
+      edges {
+        node {
+          name
+          strapiId
+          url
+          img_url
+          description
+          programming_languages {
+            name
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
